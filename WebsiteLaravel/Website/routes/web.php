e9f6bfd34992a0eccd2bf2639a036b5d71128967
Omega <omega@gmail.com>
2024-03-01 01:44:51 +0000

    Laravel Authentication

--- WebsiteLaravel/Website/routes/web.php
+++ WebsiteLaravel/Website/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,21 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
-Route::get('login', function () {
-    return view('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('register', function () {
-    return view('register');
-});
-
-Route::get('home', function () {
-    return view('information');
-});
-
-Route::get('courses', function () {
-    return view('course');
-});
+require __DIR__.'/auth.php';
